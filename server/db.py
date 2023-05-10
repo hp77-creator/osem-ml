df18910cd5bc2c3c30d037079b269d2891301727
Himanshu Pandey <himanshu.dn.pandey@gmail.com>
2023-05-10 09:20:17 +0530

    add function to save json file

--- server/db.py
+++ server/db.py
@@ -1,5 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, db
+import json
 
 cred = credentials.Certificate("secret.json")
 databaseURL = "https://finalyearproject-d2b17-default-rtdb.firebaseio.com/"
@@ -8,6 +9,12 @@ firebase_admin.initialize_app(cred, {
     })
 
 
-ref = db.reference("/")
+ref = db.reference("/users_v1/Dk1Z5a2Tdxe0jx1njiLlsEdv10t1/") # only get data of users device
 
-print(ref.get())
+json_obj = ref.get()
+
+with open('data.json', 'w') as f:
+    json.dump(json_obj, f)
+
+
+
